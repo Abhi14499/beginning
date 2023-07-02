9671e64434eb2f6ab47b0973ca6bfda1f4703ecf
Abhi14499 <abhimishra14499@gmail.com>
2023-07-03 01:16:28 +0530

    task 5 third part

--- task5(i).js
+++ task5(i).js
@@ -1,5 +1,10 @@
 //var secondItem = document.querySelector('.list-group-item:nth-child(2)');
 //secondItem.style.color = 'green';
 
-var secondItem = document.querySelectorAll('.list-group-item');
-secondItem[1].style.color = 'green';
+//var secondItem = document.querySelectorAll('.list-group-item');
+//secondItem[1].style.color = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for (var i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = 'green';
+}
