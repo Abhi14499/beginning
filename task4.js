af7e73a0cc7a0609b1f6b2b3fff7f8ba0b211ed0
Abhi14499 <abhimishra14499@gmail.com>
2023-07-02 17:47:46 +0530

    task 4 js

---
+++ task4.js
@@ -0,0 +1,12 @@
+var li = document.getElementsByTagName('li');
+for (var i = 0; i < li.length; i++) {
+    li[i].style.backgroundColor='#f4f4f4';
+    
+}
+li[4].style.listStyleType = "none";
+li[4].style.padding='15px';
+li[4].style.paddingLeft='25px';
+li[4].style.textDecoration = 'none';
+li[4].style.border='1px solid #d4d4d4';
+li[4].style.borderBottomLeftRadius='5px';
+li[4].style.borderBottomRightRadius='5px';
