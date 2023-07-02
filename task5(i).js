7cb8eb1023973fc8d51db0321c7b8f0fde3fd0b6
Abhi14499 <abhimishra14499@gmail.com>
2023-07-03 01:08:18 +0530

    task 5 second part

--- task5(i).js
+++ task5(i).js
@@ -1,2 +1,5 @@
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.color = 'green';
+//var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+//secondItem.style.color = 'green';
+
+var secondItem = document.querySelectorAll('.list-group-item');
+secondItem[1].style.color = 'green';
