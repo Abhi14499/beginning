e26d180aed9881b6b44a3a449f60d9a3371d5765
Abhi14499 <abhimishra14499@gmail.com>
2023-07-03 10:01:08 +0530

    task 7 parentElement

---
+++ task7.js
@@ -0,0 +1,19 @@
+// parentElement
+// lastelementchild
+// lastchild
+// createchild
+// firstelementchild
+// firstchild
+// nextsibling
+// nextelementsibling
+// previoussibling
+// previouselementsibling
+// createelement
+// setAttribute
+// createtesxtnode
+// appendchild
+
+var itemList = document.querySelector('#items');
+
+// parentElement
+itemList.parentElement.style.backgroundColor = '#f4f4f4';
