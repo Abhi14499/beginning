5e55da67205a07b83c642f53caab800aaf0cbe95
Abhi14499 <abhimishra14499@gmail.com>
2023-07-03 11:38:23 +0530

    task 7 adding HEllo word before Item 1

--- task7.js
+++ task7.js
@@ -45,29 +45,46 @@ var itemList = document.querySelector('#items');
 // console.log(itemList.previousElementSibling);
 
 // Create a div
-var newDiv = document.createElement('div');
+// var newDiv = document.createElement('div');
 
-// Add class
-newDiv.className = 'hello';
+// // Add class
+// newDiv.className = 'hello';
 
-// Add id
-newDiv.id = 'hello1';
+// // Add id
+// newDiv.id = 'hello1';
 
-// Add attributes
-newDiv.setAttribute('title', 'Hello Div');
+// // Add attributes
+// newDiv.setAttribute('title', 'Hello Div');
 
-// Create Text Node
-var newDivText = document.createTextNode('HEllo');
+// // Create Text Node
+// var newDivText = document.createTextNode('HEllo');
 
-// Add Text Div
-newDiv.appendChild(newDivText);
+// // Add Text Div
+// newDiv.appendChild(newDivText);
 
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
 
-container.insertBefore(newDiv, h1);
+// container.insertBefore(newDiv, h1);
 
-newDiv.style.fontSize = '30px';
+// newDiv.style.fontSize = '30px';
+
+
+    // Create a new list item element
+    var listItem = document.createElement('li');
+    listItem.className = 'list-group-item';
+    listItem.textContent = 'HEllo';
+  
+    // Find the items list element using querySelector
+    var itemsList = document.querySelector('#items');
+  
+    // Find the first list item element within the items list
+    var firstItem = itemsList.querySelector('li');
+  
+    // Insert the new list item element before the first item
+    itemsList.insertBefore(listItem, firstItem);
+  
+  
 
 
 
